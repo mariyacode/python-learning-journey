a80428569efaffb3e1a001817949e59bcc06f526
Mariya Eldhose <mariyaeldhose003@gmail.com>
2026-08-24 17:40:24 +0530

    Add integers practice

---
+++ Day-01-Basics/integers.py
@@ -0,0 +1,26 @@
+num = 3
+print(type(num))
+a = 3.24
+print(type(a))
+print(3+2)
+print(3/2)
+print(3//2)
+print(3**2)
+print(3%2)
+print(3*2+1)
+num = 1
+num *= 10
+print(num)
+print(abs(-3))
+print(abs(-5))
+print(round(3.75))
+print(round(3.75,1))
+num_1 = 3
+num_2 = 2
+print(num_1 == num_2)
+num1='200'
+num2='300'
+print(num1+num2)
+num1=int(num1)
+num2=int(num2)
+print(num1+num2)
